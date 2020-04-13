859a3195aff67aa941aa3f5f89865dc350cc72b7
Mark Grigore <edwmrkg@gmail.com>
2020-04-13 11:26:41 +0300

    rezolvare B2

--- app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/MainActivity.java
@@ -11,12 +11,22 @@ import android.widget.TextView;
 import java.io.BufferedReader;
 
 public class MainActivity extends AppCompatActivity {
-    public TextView pressedButtonsText;
+    private TextView pressedButtonsText;
+    private int buttonCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey("COUNTER")) {
+                buttonCounter = savedInstanceState.getInt("COUNTER");
+            }
+            else {
+                buttonCounter = 0;
+            }
+        }
 
         pressedButtonsText = (TextView) findViewById(R.id.pressedButtonsText);
         Button northButton = (Button)findViewById(R.id.northButton);
@@ -26,25 +36,39 @@ public class MainActivity extends AppCompatActivity {
         Button navigateToButton = (Button)findViewById(R.id.navigateToActivityButton);
 
         pressedButtonsText.setText("");
-        ButtonClickListener buttonClickListener = new ButtonClickListener(pressedButtonsText);
+        ButtonClickListener buttonClickListener = new ButtonClickListener(pressedButtonsText, buttonCounter);
         northButton.setOnClickListener(buttonClickListener);
         westButton.setOnClickListener(buttonClickListener);
         eastButton.setOnClickListener(buttonClickListener);
         southButton.setOnClickListener(buttonClickListener);
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("COUNTER", buttonCounter);
+    }
 
-
-
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState.containsKey("COUNTER")) {
+            buttonCounter = savedInstanceState.getInt("COUNTER");
+        }
+        else
+            buttonCounter = 0;
+    }
 }
 
 
 class ButtonClickListener implements View.OnClickListener {
 
     private TextView pressedText;
+    private int counter;
 
-    ButtonClickListener(TextView pressedText) {
+    ButtonClickListener(TextView pressedText, int counter) {
         this.pressedText = pressedText;
+        this.counter = counter;
     }
 
     @Override
@@ -52,18 +76,22 @@ class ButtonClickListener implements View.OnClickListener {
         switch(v.getId()) {
             case R.id.northButton:
                 pressedText.setText(pressedText.getText() + " North");
+                counter++;
                 break;
 
             case R.id.westButton:
                 pressedText.setText(pressedText.getText() + " West");
+                counter++;
                 break;
 
             case R.id.eastButton:
                 pressedText.setText(pressedText.getText() + " East");
+                counter++;
                 break;
 
             case R.id.southButton:
                 pressedText.setText(pressedText.getText() + " South");
+                counter++;
                 break;
         }
     }
